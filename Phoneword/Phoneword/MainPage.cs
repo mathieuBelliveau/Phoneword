caa91f490f397de15f6c5e8e6e506690e45edbb2
mathieuBelliveau <36576490+mathieuBelliveau@users.noreply.github.com>
2021-06-01 15:38:01 -0300

    Update UI

--- Phoneword/Phoneword/MainPage.cs
+++ Phoneword/Phoneword/MainPage.cs
@@ -9,6 +9,10 @@ namespace Phoneword
 {
     public class MainPage : ContentPage
     {
+        Entry phoneNumberText;
+        Button translateButton;
+        Button callButton;
+        string translatedNumber;
         public MainPage()
         {
             this.Padding = new Thickness(20);
@@ -34,13 +38,33 @@ namespace Phoneword
                 Text = "Translate"
             });
 
-            panel.Chilrdren.Add(callButton = new Button
+            panel.Children.Add(callButton = new Button
             {
                 Text = "Call",
                 IsEnabled = false
             });
 
+            
+            //Why no arguments? why +=?
+            translateButton.Clicked += OnTranslate;
             this.Content = panel;
+        }
+
+        private void OnTranslate(object sender, EventArgs e)
+        {
+            string enteredNumber = phoneNumberText.Text;
+            translatedNumber = Phoneword.PhonewordTranslator.ToNumber(enteredNumber);
+
+            if (!string.IsNullOrEmpty(translatedNumber))
+            {
+                callButton.IsEnabled = true;
+                callButton.Text = "Call " + translatedNumber;
+            }
+            else
+            {
+                callButton.IsEnabled = false;
+                callButton.Text = "Call";
+            }
         }
     }
 }
